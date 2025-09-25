af275eae41cd183c8651dbceae963768706e4b62
Jeremy Boy <je.boy@uni-luebeck.de>
2025-09-25 11:52:33 +0200

    Use thiserror for lpfs errs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg(any(target_os = "linux", target_os = "android"))]
 
+use thiserror::Error;
+
 /// define some macro_rule used in this crate.
 #[macro_use]
 mod macros;
@@ -10,11 +12,14 @@ pub mod proc;
 
 /// all the funcitons return this error in the crate.
 /// Any Err should be considered as an bug except `FILE_NOE_FOUND`.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ProcErr {
     /// Failed to read the corresponding file.
+    #[error("IO error: {0}")]
     IO(std::io::Error),
+    #[error("Parse error: {0:?}")]
     Parse(Box<dyn std::error::Error>),
+    #[error("Bad format: {0}")]
     BadFormat(String),
 }
 
